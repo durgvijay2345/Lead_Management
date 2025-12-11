8c7276baf066005746c1b9dfaf26e2bcc7710d70
Durgvijay Tiwari <152290653+durgvijay2345@users.noreply.github.com>
2025-12-11 15:59:19 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
 
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(` MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(" MongoDB Connection Error:", err.message);
   }
